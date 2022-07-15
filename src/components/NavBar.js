ae7f0aa8fa635ff75ec97684e7f9062ec74486cd
SallyMulupi <mulupisally2gmail.com>
2022-07-15 18:28:36 +0300

    linked home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 function NavBar() {
-
-}
+    const links = ["home",
+        "about", "project"
+    ];
+    return ( < nav > { /* display an <a> tag for each link here */ } <
+        a href = "#home" > { links[0] } < /a>
+    }
