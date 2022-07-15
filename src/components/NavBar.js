dc42c1a891bc792f10dc545c7b94944b8c85d807
SallyMulupi <mulupisally2gmail.com>
2022-07-15 18:30:00 +0300

    linked about

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,7 @@ function NavBar() {
     const links = ["home",
         "about", "project"
     ];
-    return ( < nav > { /* display an <a> tag for each link here */ } <
-        a href = "#home" > { links[0] } < /a>
-    }
+    return( <nav>{/* display an <a> tag for each link here */}
+         <a href="#home">{links[0]}</a>
+         <a href="#about">{links[1]}</a>
+         
